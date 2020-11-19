e5f2f8ec3cb60014a9be736c2c296116530be896
boojongmin <boojongmin@gmail.com>
2020-11-19 14:53:27 +0900

    update

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var s = document.createElement('script');
+s.src = "https://www.googletagmanager.com/gtag/js?id=G-G3XWW8292Y";
+s.onload = function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-G3XWW8292Y');
+}
+
+document.body.appendChild(s); 
